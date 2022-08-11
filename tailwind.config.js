65772d8be616868c9f0d8a96b7a55bd340794a7f
Matthew P <matthew.pecce@protonmail.com>
2022-08-11 05:26:54 -0400

    Commit1
    FB Mobile Done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        facebook1: '#4F68AF',
+        facebook2: '#38487D'
+      }
+    },
+  },
+  plugins: [],
+}
